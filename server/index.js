920031476c6f621a849ac61f03edeb32482cba6f
Sukant Gupta <guptasukant2@gmail.com>
2024-02-21 00:14:50 +0530

    Setup the frontend on the Express Server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
-import { createServer } from 'http'
-import { Server } from 'socket.io'
+// import { createServer } from 'http'
+import express from "express";
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const httpServer = createServer()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const io = new Server(httpServer, {
+const PORT = process.env.PORT || 3000;
+// const httpServer = createServer()
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+// * With this we can run the app and the backend both on the express server
+
+const expressServer = app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
+
+// const io = new Server(httpServer, {
+const io = new Server(expressServer, {
 	cors: {
 		origin:
 			process.env.NODE_ENV === "production"
@@ -12,19 +29,22 @@ const io = new Server(httpServer, {
 	},
 });
 
+// * The WebSocket protocol is a low-level protocol that allows us to send and receive messages
 // * ws cannot deal with the request and response cycle of the HTTP protocol
 // * It is a low-level protocol that allows us to send and receive messages
 // * It does not broadcast messages to all connected clients
-io.on('connection', socket=>{
-    console.log('New connection')
-    console.log(`User: ${socket.id} connected`)
 
-    socket.on("message", (data) => {
-        console.log(data);
-        io.emit('message', `${socket.id.substring(0, 5)}:  ${data}`)
-    })
-})
+io.on("connection", (socket) => {
+	console.log("New connection");
+	console.log(`User: ${socket.id} connected`);
 
-httpServer.listen(3000, ()=>{
-    console.log('Listening on port 3000')
-})
+	socket.on("message", (data) => {
+		console.log(data);
+		io.emit("message", `${socket.id.substring(0, 5)}:  ${data}`);
+	});
+});
+
+// httpServer.listen(3000, ()=>{
+//     console.log('Listening on port 3000')
+// })
+
